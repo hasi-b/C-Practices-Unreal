b5ec0201f2f69bdee80563983e82fed3b08c4e5d
Hasi-b <hasib.hasan9@gmail.com>
2021-06-01 02:40:59 +0600

    Pure basics

---
+++ tripleX.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+void playGame();
+
+int main() 
+{
+    std::cout <<"World is about to destroy";
+    std::cout << std::endl;
+    std::cout <<"Enter the correct Code to hit Meteor Co-ordinate";
+    int a=4,b=5,c=10;
+    const int sum =a+b+c;
+    std::cout<<std::endl;
+    std::cout<<sum<<std::endl;
+    playGame();
+    return 0;
+}
+void playGame()
+{
+    std::cout<<"I gave up this section of the tut cause it is too easy, gonna start from the next section";
+}
